8fa3618e5cc3197246f4693c2af75c9100e8ae3d
dmurari7 <dharshaan@hotmail.com>
2026-01-11 19:45:04 -0400

    completed Task C; get game stats for all players in the last 30 days

--- streamingOptions.py
+++ streamingOptions.py
@@ -1,12 +1,18 @@
 from nba_api.stats.endpoints import ScoreboardV2
-from datetime import date
+from nba_api.stats.endpoints import PlayerGameLogs
+from datetime import date, timedelta
 import pandas as pd
 
 today = date.today()
 
+test_date_to = '12/15/2024'  
+test_date_from = '11/15/2024'  
+
 scoreboard = ScoreboardV2(game_date=today.strftime('%m/%d/%Y'))
+player_logs = PlayerGameLogs(season_nullable='2024-25', date_from_nullable=test_date_from, date_to_nullable=test_date_to)
 
 games_df = scoreboard.get_data_frames()[0]
+players_df = player_logs.get_data_frames()[0]
  
 home_team_ids = games_df['HOME_TEAM_ID']
 visitor_team_ids = games_df['VISITOR_TEAM_ID']
@@ -14,3 +20,4 @@ visitor_team_ids = games_df['VISITOR_TEAM_ID']
 team_ids = pd.concat([home_team_ids, visitor_team_ids]).unique().tolist()
 
 print(team_ids)
+print(players_df.head())
